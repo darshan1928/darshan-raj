e5347610e772caa9584dcf3a15bf75b9a79df04f
Darshan <darshankollam8@gmail.com>
2023-03-11 13:44:53 +0530

    commitsecond

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
     <>
     <header>
       <div className="container header__container">
-      <h3>Hey,I'm</h3>
+      <h3>Hey, I'm</h3>
       <h1>DARSHAN RAJ</h1>
       <div className="text-light"> <h3>FULL-STACK DEVELOPER</h3></div>
     
